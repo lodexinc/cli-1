3409fd8f293b700faa0282bf5f5816b1ecdc792a
Matthew Boedicker <mboedicker@pivotal.io>
2017-03-07 10:06:29 -0800

    Add function for sorting and uniquifying string slices
    [#126256681]
    
    Signed-off-by: Stu Pollock <spollock@pivotal.io>

--- util/generic/slice.go
+++ util/generic/slice.go
@@ -1,10 +1,28 @@
 package generic
 
-import "reflect"
+import (
+	"reflect"
+	"sort"
+)
 
 func IsSliceable(value interface{}) bool {
 	if value == nil {
 		return false
 	}
 	return reflect.TypeOf(value).Kind() == reflect.Slice
+}
+
+func SortAndUniquifyStringSlice(slice []string) []string {
+	sort.Strings(slice)
+
+	var newSlice []string
+
+	for _, str := range slice {
+		currentLength := len(newSlice)
+		if currentLength == 0 || newSlice[currentLength-1] != str {
+			newSlice = append(newSlice, str)
+		}
+	}
+
+	return newSlice
 }
